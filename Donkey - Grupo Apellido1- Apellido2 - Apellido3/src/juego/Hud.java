8791351308da91ae84b73a90c3e6afa0acaf5cc3
Damian Lisas <dergonokay@gmail.com>
2017-10-20 14:53:12 -0300

    asd

--- Donkey - Grupo Apellido1- Apellido2 - Apellido3/src/juego/Hud.java
+++ Donkey - Grupo Apellido1- Apellido2 - Apellido3/src/juego/Hud.java
@@ -4,48 +4,7 @@ import entorno.Entorno;
 
 public class Hud 
 {
-	private int vidas;
-	private int tiempo;
-	private int puntos;
-	
-	Hud(int vidas, int tiempo, int puntos)
-	{
-		this.setVidas(vidas);
-		this.setTiempo(tiempo);
-		this.setPuntos(puntos);
-	}
-
-	public int getPuntos() 
-	{
-		return this.puntos;
-	}
-
-	public void setPuntos(int puntos) 
-	{
-		this.puntos = puntos;
-	}
-
-	public int getTiempo() 
-	{
-		return this.tiempo;
-	}
-
-	public void setTiempo(int tiempo) 
-	{
-		this.tiempo = tiempo;
-	}
-
-	public int getVidas() 
-	{
-		return this.vidas;
-	}
-
-	public void setVidas(int vidas) 
-	{
-		this.vidas = vidas;
-	}
-	
-	public void dibujarse(Entorno entorno,int vidas, int tiempo, int puntos)
+	public static void dibujarse(Entorno entorno,int vidas, int tiempo, int puntos)
 	{
 		entorno.escribirTexto("VIDAS: " + vidas, 40, 30);
 		entorno.escribirTexto("TIEMPO: " + tiempo,  entorno.ancho()/2-40, 30);
